f2a598d3af808d998b4d2e444d888a7dfcc78bca
Gabriel Wagner <gabrielreyeswagner@gmail.com>
2025-03-06 00:26:19 -0500

    Lowered game res for web + lighting effects

--- Shaders/main_shader_canvas.gd
+++ Shaders/main_shader_canvas.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+@export var max_drunk_effect_multiplier = 8
+
+
 ##dictionary is as follows: String:Array[Control,Rectangle(ShaderA),Rectangle(ShaderB),...]
 @onready var filter_dict : Dictionary = {"drunk":[$Drunk,$Drunk/WobbleShader,$Drunk/ChromaticShader],
 "BeerMeter":[$"Beer Meter"]}
@@ -16,6 +19,11 @@ func get_shaders(key:String)->Array[ShaderMaterial]:
 			if elem.material is ShaderMaterial:
 				temp.append(elem.material)
 	return temp
+
+func _process(delta: float) -> void:
+	var drunk_percent = Globals.drunkenness / 100.0
+	var shader_intensity = get_shaders("drunk")[0].get_shader_parameter("drunkScale")
+	get_shaders("drunk")[0].set_shader_parameter("drunkScale", move_toward(shader_intensity, drunk_percent, delta * 0.1))
 
 func _ready():
 	var shader_mats = get_shaders("drunk")[0].shader.get_shader_uniform_list()
