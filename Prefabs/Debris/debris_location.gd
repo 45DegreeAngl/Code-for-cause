bce51737184f4a9db384633cef0d83418f01c38e
drw_xd <drw2007@icloud.com>
2025-03-06 19:21:07 -0500

    Added Debris Logic

--- Prefabs/Debris/debris_location.gd
+++ Prefabs/Debris/debris_location.gd
@@ -3,18 +3,49 @@ class_name DebrisLocation
 
 @export var accepts_large : bool = true
 @export var accepts_small : bool = true
-@export var small_debris : Array[DebrisGroup]
-@export var large_debris : Array[DebrisGroup]
+@export var small_debris : Array[PackedScene]
+@export var large_debris : Array[PackedScene]
 
 @export_subgroup("Settings")
+@export_range(0,1,0.01) var spawn_chance : float = 1
 @export_range(0,1,0.01) var small_spawn_chance : float = 0.70
 
 @export var use_same_debris_type : bool = true
 @export var small_spawn_count : Vector2i = Vector2i.ONE
 @export var large_spawn_count : Vector2i = Vector2i.ONE
 
+@export_subgroup("Spawn Markers")
+@export var small_locations : Array[Marker3D]
+@export var large_locations : Array[Marker3D]
+
 func spawn_debris():
+	if randf() <= 1-spawn_chance:
+		return
 	if randf() <= small_spawn_chance:
-		pass
+		var count = randi_range(small_spawn_count.x, small_spawn_count.y)
+		var temp_list = small_locations.duplicate()
+		if use_same_debris_type:
+			var type = small_debris.pick_random()
+			for i in range(count):
+				var loc = temp_list.pick_random()
+				loc.add_child(type.instantiate())
+				temp_list.erase(loc)
+		else:
+			for i in range(count):
+				var loc = temp_list.pick_random()
+				loc.add_child(small_debris.pick_random().instantiate())
+				temp_list.erase(loc)
 	else:
-		pass
+		var count = randi_range(large_spawn_count.x, large_spawn_count.y)
+		var temp_list = large_locations.duplicate()
+		if use_same_debris_type:
+			var type = large_debris.pick_random()
+			for i in range(count):
+				var loc = temp_list.pick_random()
+				loc.add_child(type.instantiate())
+				temp_list.erase(loc)
+		else:
+			for i in range(count):
+				var loc = temp_list.pick_random()
+				loc.add_child(large_debris.pick_random().instantiate())
+				temp_list.erase(loc)
--- Prefabs/Roads/road_segment.gd
+++ Prefabs/Roads/road_segment.gd
@@ -3,10 +3,13 @@ class_name RoadSegment
 
 var index : int
 
-@export var debris_locations : Array[DebrisLocation]
+@export var debris_node : Node3D
+@export var max_debris : int = 10
+
 
 func _ready():
-	pass
+	spawn_debris()
 
-func spawn_debris(location : DebrisLocation) -> Node3D:
-	return null
+func spawn_debris():
+	for child in debris_node.get_children():
+		child.spawn_debris()
